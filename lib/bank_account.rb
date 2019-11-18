a3ce4ad825a62acec3a15e2577e4992de98d4601
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-11-18 22:45:14 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open" # When an account is created, it is considered to be open
+  end
+
+  # Instance method we use when depositing money
+  def deposit(money)
+    @balance = @balance + money #=> 1000 + $$$
+  end
+
+  # Displays the balance currently within our account
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  # Determines if account is valid
+  # To be valid, the status must be open and the balance greater than zero
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,47 @@
 class Transfer
-  # your code here
+
+  require 'pry'
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  # Each transfer MUST have a sender, receiver and amount
+  # Each instance also has a default status of "pending"
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  # Checks to see if the sender and receiver have valid accounts
+  # Both MUST be valid
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if valid? && status == "pending"
+      if amount < self.sender.balance
+        self.sender.balance -= amount
+        self.receiver.balance += amount
+        self.status = "complete"
+      else
+         self.status = "rejected"
+         "Transaction rejected. Please check your account balance."
+      end
+    else
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  # Reverses a completed transfer
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount # Refunds the sender
+      @receiver.balance -= @amount # Takes from the original receiver
+      @status = "reversed" # Transaction is complete
+    end
+  end
+
 end
